6dd139f9ea2ac555a7045e8c1bae7ab52d7a20a8
Bess Sadler <bess@curationexperts.com>
2018-07-31 16:32:40 -0400

    Imported works must have a depositor
    Works won't display without a depositor.
    For now, assign the User.batch_user,
    a utility user account that ships with
    Hyrax and is handy for situations like
    this.

--- app/ingest/mods_mapper.rb
+++ app/ingest/mods_mapper.rb
@@ -6,12 +6,16 @@ class ModsMapper < Darlingtonia::MetadataMapper
   NAMESPACES = { 'mods' => 'http://www.loc.gov/mods/v3' }.freeze
 
   def fields
-    [:title]
+    [:title, :depositor]
   end
 
   def title
     metadata
       &.xpath('//mods:mods/mods:titleInfo/mods:title', NAMESPACES)
       &.map(&:text) || []
+  end
+
+  def depositor
+    User.batch_user.user_key
   end
 end
