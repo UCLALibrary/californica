789a707718ab7b4b515a8527e039994a6b59ccbe
Hardy Pottinger <hardy.pottinger@gmail.com>
2018-08-02 11:57:57 -0500

    Add Genre
    Add property to work
    Add test
    (with help from Bess)

--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -4,7 +4,10 @@
 require 'rails_helper'
 
 RSpec.describe Work do
-  it "has tests" do
-    skip "Add your tests here"
+  it "has genre" do
+    work = described_class.new
+    work.genre = ['SciFi']
+    expect(work.genre).to include 'SciFi'
+    expect(work.resource.dump(:ttl)).to match(/purl.org\/dc\/elements\/1.1\/type/)
   end
 end
