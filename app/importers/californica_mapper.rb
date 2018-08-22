c608a94d7d31ed7d706688e7d5424e4ff4bf11c3
Tom Johnson <tom@curationexperts.com>
2018-08-22 12:15:33 -0700

    Setup mapper to read delimited data

--- app/importers/californica_mapper.rb
+++ app/importers/californica_mapper.rb
@@ -7,24 +7,15 @@ class CalifornicaMapper < Darlingtonia::HashMapper
     subject: "Subject"
   }.freeze
 
-  def fields
-    CALIFORNICA_TERMS_MAP.keys.reject do |e|
-      next if e == :publisher_name
-      metadata[CALIFORNICA_TERMS_MAP[e]].nil?
-    end
-  end
+  DELIMITER = '|~|'
 
-  def representative_file
-    @metadata['file_name']
+  def fields
+    CALIFORNICA_TERMS_MAP.keys
   end
 
   def map_field(name)
     return unless CALIFORNICA_TERMS_MAP.keys.include?(name)
 
-    # Multivalue fields can be present in rows. When they occur, they have integers appended to them: name1, name2.
-    # We check for multiple occurances of the name and integers (optionally), and return the values in an array.
-    metadata.select do |k, val|
-      val if k.match?(/^#{CALIFORNICA_TERMS_MAP[name]}[0-9]*$/)
-    end.values
+    metadata[name].split(DELIMITER)
   end
 end
--- spec/importers/californica_mapper_spec.rb
+++ spec/importers/californica_mapper_spec.rb
@@ -5,10 +5,10 @@ require 'rails_helper'
 RSpec.describe CalifornicaMapper do
   subject(:mapper) { described_class.new }
 
-  let(:hyrax_metadata) do
-    { identifier: ["21198/zz0002nq4w"],
-      title: ["Protesters with signs in gallery of Los Angeles County Supervisors " \
-      "hearing over eminent domain for construction of Harbor Freeway, Calif., 1947"],
+  let(:metadata) do
+    { identifier: "21198/zz0002nq4w",
+      title: "Protesters with signs in gallery of Los Angeles County Supervisors " \
+      "hearing over eminent domain for construction of Harbor Freeway, Calif., 1947",
       type: "still image",
       subject: "Express highways--California--Los Angeles County--Design and construction|~|" \
       "Eminent domain--California--Los Angeles|~|Demonstrations--California--Los Angeles County|~|" \
@@ -18,15 +18,15 @@ RSpec.describe CalifornicaMapper do
       repository_name: "University of California, Los Angeles. $b Library Special Collections" }
   end
 
+  before { mapper.metadata = metadata }
+
   it "maps the required title field" do
-    mapper.metadata = { "Title" => "Protesters with signs in gallery of Los Angeles County Supervisors " \
-      "hearing over eminent domain for construction of Harbor Freeway, Calif., 1947" }
-    expect(mapper.map_field(:title)).to eq(["Protesters with signs in gallery of Los Angeles County Supervisors " \
-    "hearing over eminent domain for construction of Harbor Freeway, Calif., 1947"])
+    expect(mapper.map_field(:title))
+      .to contain_exactly("Protesters with signs in gallery of Los Angeles County Supervisors " \
+                          "hearing over eminent domain for construction of Harbor Freeway, Calif., 1947")
   end
 
   it "maps the required identifier field" do
-    mapper.metadata = { "Item Ark" => "21198/zz0002nq4w" }
-    expect(mapper.map_field(:identifier)).to eq(["21198/zz0002nq4w"])
+    expect(mapper.map_field(:identifier)).to contain_exactly('21198/zz0002nq4w')
   end
 end
