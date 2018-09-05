87a96806ed8406a760c0cdcf6d24bfba93d7773f
Tom Johnson <tom@curationexperts.com>
2018-09-05 12:10:33 -0700

    Remove existing `Work#genre`
    `Work#genre` was added in 789a707718a, but has an outdated predicate and
    possibly incorrect indexing behavior. This removes it to get a clean starting
    place for adding the genre term.

--- app/models/work.rb
+++ app/models/work.rb
@@ -8,9 +8,7 @@ class Work < ActiveFedora::Base
   # Change this to restrict which works can be added as a child.
   # self.valid_child_concerns = []
   validates :title, presence: { message: 'Your work must have a title.' }
-  property :genre, predicate: 'http://purl.org/dc/elements/1.1/type' do |index|
-    index.as :stored_searchable
-  end
+
   property :extent, predicate: 'http://purl.org/dc/elements/1.1/format' do |index|
     index.as :stored_searchable
   end
--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -4,12 +4,6 @@
 require 'rails_helper'
 
 RSpec.describe Work do
-  it "has genre" do
-    work = described_class.new
-    work.genre = ['SciFi']
-    expect(work.genre).to include 'SciFi'
-    expect(work.resource.dump(:ttl)).to match(/purl.org\/dc\/elements\/1.1\/type/)
-  end
   it "has extent" do
     work = described_class.new
     work.extent = ['1 photograph']
