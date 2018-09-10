7b85eb102b6c7e341205a70771b5698ef16fabe3
Hardy Pottinger <hardy.pottinger@gmail.com>
2018-09-10 16:50:45 -0500

    Add a default-url-options :host key
    Following the example here:
    https://github.com/curationexperts/laevigata/blob/f8790934073315ad74797212263ea4120b386602/config/application.rb#L10
    
    Adding this to config/application.rb should address issue #65

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module Tenjeo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.action_mailer.default_url_options = { host: ENV["RAILS_HOST"] }
+
     config.active_job.queue_adapter = :sidekiq
   end
 end
+Rails.application.routes.default_url_options[:host] = ENV["RAILS_HOST"]
