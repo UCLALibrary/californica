a26410eed0e24a506525d9bc435776b42aa8b470
Hardy Pottinger <hardy@thebignow.com>
2018-09-12 10:03:18 -0500

    Change all references to tenejo to californica. (#124)
    Changing all references to tenejo to californica, should address the
    lingering referneces to Tenejo in the web UI. Refs #99.

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Tenjeo
+module Californica
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
