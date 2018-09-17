f1b283fc6b34c9d2701aa35bac62165f25c3f2e6
Jamie Little <jamie@jamielittle.org>
2018-09-17 20:03:12 +0000

    Add custom terms to form
    This commit adds the custom terms to the `WorkForm` object.
    By adding these terms to that object, they become editable
    on the work's form in the editing interface. This also
    adds additional locale config so that they show up on
    the edit screen with the correct labels.

---
+++ spec/forms/hyrax/work_form_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Hyrax::WorkForm do
+  let(:form) { described_class.new(Work.new, {}, {}) }
+
+  it 'has all the custom terms' do
+    expect(form.terms).to include(:extent, :caption, :dimensions,
+                                  :funding_note, :genre, :latitude,
+                                  :longitude, :local_identifier, :medium,
+                                  :named_subject, :normalized_date, :repository,
+                                  :rights_country, :rights_holder)
+  end
+end
