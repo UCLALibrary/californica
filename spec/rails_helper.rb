1a7f9c7555d2f27788d7bd9c0fcfb478f8272f29
Bess Sadler <bess@curationexperts.com>
2018-09-18 15:24:33 -0400

    Initial FactoryBot factories for work and user (#194)
    Create a user and an admin user for use in tests
    Create a work factory for use in tests
    Use FFaker fake data to populate these
    Initial feature test for showing a work

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ require 'rspec/rails'
 
 require 'database_cleaner'
 require 'active_fedora/cleaner'
+require 'ffaker'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
