3d8742e5c772e6220ed36e3876962a36d21ebe84
Valerie Maher <val99erie@users.noreply.github.com>
2018-09-28 08:53:44 -0500

    Clean Ursus solr before reindexing. Part of story #214 (#235)
    I added a method to delete all the solr docs from the 2nd solr.
    
    I refactored the reindex method to pull out code that is needed in both
    methods.
    
    I updated the rake task to clean out the ursus solr before reindex.

--- app/services/reindex_service.rb
+++ app/services/reindex_service.rb
@@ -9,16 +9,33 @@ class ReindexService
     @solr = solr
   end
 
-  # Switches out the ActiveFedora::SolrService connection to a
-  # a solr server that you specify and then reindexes your
-  # repository content for that server. It then restores the original
-  # connection
-  #
+  # Reindexes your repository content to the
+  # alternate solr server.
   # @return [Boolean] the reindex success
   def reindex
-    ActiveFedora::SolrService.instance.conn = RSolr.connect url: @solr
-    result = ActiveFedora::Base.reindex_everything
-    ActiveFedora::SolrService.instance.conn = RSolr.connect url: @original_solr
+    result = nil
+    connected_to_alternate_solr do
+      result = ActiveFedora::Base.reindex_everything
+    end
     result
+  end
+
+  # This method does:
+  #   1. Switch out the ActiveFedora::SolrService
+  #      connection to the alternate solr server
+  #   2. Do some work
+  #   3. Restore the original connection
+  def connected_to_alternate_solr
+    ActiveFedora::SolrService.instance.conn = RSolr.connect url: @solr
+    yield
+    ActiveFedora::SolrService.instance.conn = RSolr.connect url: @original_solr
+  end
+
+  # Delete all documents in the alternate solr server
+  def delete_all_docs
+    connected_to_alternate_solr do
+      conn = ActiveFedora::SolrService.instance.conn
+      conn.delete_by_query("*:*", params: { commit: true })
+    end
   end
 end
