63d8472a1c4d0aa70ee6e496f8f024db16c6ae60
Bess Sadler <bess@curationexperts.com>
2018-10-19 16:02:23 -0400

    Metadata on single item view
    Feature test to check for all expected fields on a
    single item show page.

--- app/presenters/hyrax/work_presenter.rb
+++ app/presenters/hyrax/work_presenter.rb
@@ -5,7 +5,7 @@ module Hyrax
   class WorkPresenter < Hyrax::WorkShowPresenter
     delegate :resource_type, :extent, :caption, :dimensions,
              :funding_note, :genre, :location, :geographic_coordinates,
-             :medium,
+             :medium, :local_identifier,
              :named_subject, :normalized_date, :repository,
              :rights_country, :rights_holder, to: :solr_document
   end
