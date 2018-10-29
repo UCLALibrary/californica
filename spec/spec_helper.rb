a96db94010231b601299bcf254c9d06b4f36672c
jendiamond <thejendiamond@gmail.com>
2018-10-29 21:59:40 +0000

    Add Coverall
    Add coverall gem to track our test coverage over time.
    ---
    
    + modified:   Gemfile
    + modified:   Gemfile.lock
    + modified:   spec/spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,11 @@
 require 'simplecov'
 SimpleCov.start
 
+if ENV['TRAVIS'] == 'true'
+  require 'coveralls'
+  Coveralls.wear!('rails')
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
