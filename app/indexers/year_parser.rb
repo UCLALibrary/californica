6f3660e2f752a474e350d2a5c233b376814bd7bc
val99erie <val99erie@gmail.com>
2018-11-02 15:56:46 -0500

    Added test cases for year parser. Story UCLALibrary/ursus#69

--- app/indexers/year_parser.rb
+++ app/indexers/year_parser.rb
@@ -4,16 +4,50 @@ class YearParser
   # @param dates [String, Array<String>] A list of String dates
   # @return [Array<Integer>] Sorted list of years that correspond to those dates
   def self.integer_years(dates)
-    Array.wrap(dates).map do |date|
-      if has_a_year?(date)
-        Date.strptime(date, '%Y').year
-      end
-    end.compact.sort
+    Array.wrap(dates).flat_map do |date|
+      years(date) if maybe_contains_a_year?(date)
+    end.compact.uniq.sort
   end
 
   # If this string doesn't have 4 numbers in a row, it doesn't contain a 4-digit year.
-  def self.has_a_year?(input_string)
+  def self.maybe_contains_a_year?(input_string)
     four_digit_year = %r{\d{4}}
     input_string.match?(four_digit_year)
+  end
+
+  def self.years(input_string)
+    if date_range?(input_string)
+      expand_date(input_string)
+    else
+      parse_year(input_string)
+    end
+  end
+
+  # Check if it's meant to be a range of dates instead of a single date. Examples:
+  #   '1937/1939'
+  #   '1934-06/1934-07'
+  def self.date_range?(input_string)
+    input_string.match?(range_separator)
+  end
+
+  def self.range_separator
+    %r{\/} # a forward slash
+  end
+
+  # If the string is a range of dates instead of a
+  # single date, expand the range into an array of
+  # values.
+  def self.expand_date(input_string)
+    range_start = input_string.match(/^(.*)#{range_separator}/).captures.first
+    range_end = input_string.match(/^.*#{range_separator}(.*)/).captures.first
+
+    starting_year = parse_year(range_start)
+    ending_year = parse_year(range_end)
+
+    (starting_year..ending_year).to_a
+  end
+
+  def self.parse_year(date_string)
+    Date.strptime(date_string, '%Y').year
   end
 end
--- spec/indexers/year_parser_spec.rb
+++ spec/indexers/year_parser_spec.rb
@@ -5,14 +5,19 @@ RSpec.describe YearParser do
   describe '::integer_years' do
     subject { described_class.integer_years(dates) }
 
+    context 'ISO 8601 date format' do
+      let(:dates) { ['1941-10-01'] }
+      it { is_expected.to eq [1941] }
+    end
+
     context 'just the year' do
       let(:dates) { '1953' }
       it { is_expected.to eq [1953] }
     end
 
-    context 'ISO 8601 date format' do
-      let(:dates) { ['1941-10-01'] }
-      it { is_expected.to eq [1941] }
+    context 'just year and month' do
+      let(:dates) { '1953-10' }
+      it { is_expected.to eq [1953] }
     end
 
     context 'multiple dates' do
@@ -37,6 +42,25 @@ RSpec.describe YearParser do
 
       it 'returns the years that it can parse' do
         expect(parsed_dates).to eq [1953]
+      end
+    end
+
+    context 'ranges of dates' do
+      let(:dates) { ['1937/1939', '1942/1943'] }
+      it { is_expected.to eq [1937, 1938, 1939, 1942, 1943] }
+    end
+
+    context 'date ranges that aren\'t just years' do
+      let(:dates) { '1934-06/1934-07' }
+      it { is_expected.to eq [1934] }
+    end
+
+    context 'duplicate years' do
+      let(:dates) { ['1934-06/1935-07', '1934-06-01', '1934'] }
+      let(:parsed_dates) { subject }
+
+      it 'removes duplcates from the list' do
+        expect(parsed_dates).to eq [1934, 1935]
       end
     end
   end
