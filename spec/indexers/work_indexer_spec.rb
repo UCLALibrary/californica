ae6062bd74e49e44acab4e230a44c53517d6d4e4
jendiamond <thejendiamond@gmail.com>
2019-02-20 14:15:48 -0800

    Make ARK available to blacklight as single-value field
    Add "ark:/" on the value in the solr index if it doesn't already exist
    
    ---
    
    On branch single-value-ark_399
    
    Changes to be committed:
    + modified:   app/indexers/work_indexer.rb
    + deleted:    lib/tasks/.keep
    + modified:   spec/indexers/work_indexer_spec.rb

--- spec/indexers/work_indexer_spec.rb
+++ spec/indexers/work_indexer_spec.rb
@@ -84,11 +84,19 @@ RSpec.describe WorkIndexer do
   end
 
   describe 'ark' do
-    let(:attributes) { { identifier: ['123', 'ark:/67531/metadc1213294/', '456'] } }
+    let(:attributes) { { identifier: ['123/456'] } }
 
     # To be able use the ark as the Blacklight identifier we need it to be a single value string.
     it 'indexes as a single value "string"' do
-      expect(solr_document['ark_ssi']).to eq 'ark:/67531/metadc1213294/'
+      expect(solr_document['ark_ssi']).to eq 'ark:/123/456'
+    end
+  end
+
+  describe 'ark' do
+    let(:attributes) { { identifier: ['ark:/123/456'] } }
+
+    it 'does not duplicate the "ark:/"' do
+      expect(solr_document['ark_ssi']).to eq 'ark:/123/456'
     end
   end
 end
