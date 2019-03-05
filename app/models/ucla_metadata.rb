433012c501e5856327f6a6a95a2945b2d00feebe
Bess Sadler <bess@curationexperts.com>
2019-03-05 15:17:53 -0500

    Collection objects should have all metadata fields
    All UCLA metadata fields should be available to Collection objects as
    well as work objects. Refactor to put UCLA metadata configuration in an
    includable module to keep things DRY.

---
+++ app/models/ucla_metadata.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+module UclaMetadata
+  extend ActiveSupport::Concern
+
+  included do
+    property :extent, predicate: ::RDF::Vocab::DC11.format do |index|
+      index.as :stored_searchable, :facetable
+    end
+
+    property :caption, predicate: ::RDF::Vocab::SCHEMA.caption do |index|
+      index.as :stored_searchable
+    end
+
+    property :dimensions, predicate: ::RDF::Vocab::MODS.physicalExtent do |index|
+      index.as :stored_searchable, :facetable
+    end
+
+    property :dlcs_collection_name, predicate: ::RDF::URI.intern('https://bib.schema.org/Collection') do |index|
+      index.as :displayable, :facetable
+    end
+
+    property :funding_note, predicate: ::RDF::URI.intern('http://bibfra.me/vocab/marc/fundingInformation') do |index|
+      index.as :stored_searchable
+    end
+
+    property :genre, predicate: ::RDF::Vocab::EDM.hasType do |index|
+      index.as :stored_searchable, :facetable
+    end
+
+    property :latitude, predicate: ::RDF::Vocab::EXIF.gpsLatitude do |index|
+      index.as :stored_searchable
+    end
+
+    property :location, predicate: ::RDF::Vocab::DC.coverage do |index|
+      index.as :stored_searchable, :facetable
+    end
+
+    property :local_identifier, predicate: ::RDF::Vocab::DC11.identifier do |index|
+      index.as :stored_searchable
+    end
+
+    property :longitude, predicate: ::RDF::Vocab::EXIF.gpsLongitude do |index|
+      index.as :stored_searchable
+    end
+
+    property :medium, predicate: ::RDF::Vocab::DC.medium do |index|
+      index.as :stored_searchable, :facetable
+    end
+
+    property :named_subject, predicate: ::RDF::Vocab::MODS.subjectName do |index|
+      index.as :stored_searchable, :facetable
+    end
+
+    property :normalized_date, predicate: ::RDF::Vocab::DC11.date do |index|
+      index.as :stored_searchable, :facetable
+    end
+
+    property :photographer, predicate: ::RDF::URI.intern('http://id.loc.gov/vocabulary/relators/pht.html') do |index|
+      index.as :stored_searchable, :facetable
+    end
+
+    property :repository, predicate: ::RDF::Vocab::MODS.locationCopySublocation do |index|
+      index.as :stored_searchable
+    end
+
+    property :rights_country, predicate: ::RDF::Vocab::EBUCore.rightsType do |index|
+      index.as :stored_searchable
+    end
+
+    property :rights_holder, predicate: ::RDF::Vocab::EBUCore.hasRightsHolder do |index|
+      index.as :stored_searchable
+    end
+  end
+end
---
+++ spec/models/collection_spec.rb
@@ -0,0 +1,118 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Collection do
+  subject(:collection) { described_class.new }
+
+  describe '#genre' do
+    let(:values) { ['SciFi'] }
+
+    it 'can set a genre' do
+      expect { collection.genre = values }
+        .to change { collection.genre.to_a }
+        .to contain_exactly(*values)
+    end
+
+    it 'sets to edm:hasType' do
+      expect { collection.genre = values }
+        .to change { collection.resource.predicates }
+        .to include RDF::Vocab::EDM.hasType
+    end
+  end
+
+  it "has publisher" do
+    collection.publisher = ['publisher']
+    expect(collection.publisher).to include 'publisher'
+    expect(collection.resource.dump(:ttl)).to match(/purl.org\/dc\/elements\/1.1\/publisher/)
+  end
+
+  it "has extent" do
+    collection.extent = ['1 photograph']
+    expect(collection.extent).to include '1 photograph'
+    expect(collection.resource.dump(:ttl)).to match(/purl.org\/dc\/elements\/1.1\/format/)
+  end
+
+  it "has repository" do
+    collection.repository = ['a repository']
+    expect(collection.repository).to include 'a repository'
+    expect(collection.resource.dump(:ttl)).to match(/loc.gov\/mods\/rdf\/v1#locationCopySublocation/)
+  end
+
+  it "has normalized date" do
+    collection.normalized_date = ['01/01/01']
+    expect(collection.normalized_date).to include '01/01/01'
+    expect(collection.resource.dump(:ttl)).to match(/purl.org\/dc\/elements\/1.1\/date/)
+  end
+
+  it "has local_identifier" do
+    collection.local_identifier = ['local_identifier']
+    expect(collection.local_identifier).to include 'local_identifier'
+    expect(collection.resource.dump(:ttl)).to match(/purl.org\/dc\/elements\/1.1\/identifier/)
+  end
+
+  it "has funding_note" do
+    collection.funding_note = ['funding_note']
+    expect(collection.funding_note).to include 'funding_note'
+    expect(collection.resource.dump(:ttl)).to match(/bibfra.me\/vocab\/marc\/fundingInformation/)
+  end
+
+  it "has latitude" do
+    collection.latitude = ['39']
+    expect(collection.latitude).to include '39'
+    expect(collection.resource.dump(:ttl)).to match(/w3.org\/2003\/12\/exif\/ns#gpsLatitude/)
+  end
+
+  it "has longitude" do
+    collection.longitude = ['-94']
+    expect(collection.longitude).to include '-94'
+    expect(collection.resource.dump(:ttl)).to match(/w3.org\/2003\/12\/exif\/ns#gpsLongitude/)
+  end
+
+  it "has named subject" do
+    collection.named_subject = ['Person, A']
+    expect(collection.named_subject).to include 'Person, A'
+    expect(collection.resource.dump(:ttl)).to match(/loc.gov\/mods\/rdf\/v1#subjectName/)
+  end
+
+  it "rights country" do
+    collection.rights_country = ['rights_country']
+    expect(collection.rights_country).to include 'rights_country'
+    expect(collection.resource.dump(:ttl)).to match(/ebu.ch\/metadata\/ontologies\/ebucore\/ebucore#rightsType/)
+  end
+
+  it "has rights holder" do
+    collection.rights_holder = ['rights_holder']
+    expect(collection.rights_holder).to include 'rights_holder'
+    expect(collection.resource.dump(:ttl)).to match(/ebu.ch\/metadata\/ontologies\/ebucore\/ebucore#hasRightsHolder/)
+  end
+
+  it "has medium" do
+    collection.medium = ['Capacitance Electronic Disc']
+    expect(collection.medium).to include 'Capacitance Electronic Disc'
+    expect(collection.resource.dump(:ttl)).to match(/purl.org\/dc\/terms\/medium/)
+  end
+
+  it "has dimensions" do
+    collection.dimensions = ['2x4']
+    expect(collection.dimensions).to include '2x4'
+    expect(collection.resource.dump(:ttl)).to match(/loc.gov\/mods\/rdf\/v1#physicalExtent/)
+  end
+
+  it "has caption" do
+    collection.caption = ['a caption']
+    expect(collection.caption).to include 'a caption'
+    expect(collection.resource.dump(:ttl)).to match(/schema.org\/caption/)
+  end
+
+  it "has location" do
+    collection.location = ['a location']
+    expect(collection.location).to include 'a location'
+    expect(collection.resource.dump(:ttl)).to match(/purl.org\/dc\/terms\/coverage/)
+  end
+
+  it "has photographer" do
+    collection.photographer = ['Ansel Adams']
+    expect(collection.photographer).to include 'Ansel Adams'
+    expect(collection.resource.dump(:ttl)).to match(/id.loc.gov\/vocabulary\/relators\/pht.html/)
+  end
+end
