de67b242d4344e7266ee9e6d7495a742e24b1dd3
Bess Sadler <bess@curationexperts.com>
2019-03-27 14:41:07 -0400

    Ark values are not editable
    Allow an Ark to be set via the new work form, but once an object has an
    ark do not allow it to be edited from the form -- make it readonly.

--- app/forms/hyrax/work_form.rb
+++ app/forms/hyrax/work_form.rb
@@ -14,5 +14,15 @@ module Hyrax
                    :rights_country, :rights_holder, :photographer]
 
     self.required_fields = [:title, :ark, :rights_statement]
+
+    # Make some fields read-only
+    def readonly?(field)
+      readonly_fields = [:ark]
+      # If there is no value set already, allow it to be set the first time
+      return false if send(field).empty?
+      # If a readonly field has a value set already, do not allow it to be edited
+      return true if readonly_fields.include?(field)
+      false
+    end
   end
 end
