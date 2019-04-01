1c50f6b5151643f67db0199ca1886a3f619f210e
Bess Sadler <bess@curationexperts.com>
2019-04-01 15:53:51 -0400

    Delete objects where ark matches

--- app/importers/californica_csv_cleaner.rb
+++ app/importers/californica_csv_cleaner.rb
@@ -33,11 +33,15 @@ class CalifornicaCsvCleaner < Darlingtonia::CsvParser
   def clean
     # use the CalifornicaMapper
     # Match on ark. For every ark in the file, find the corresponding object
-    # and destroy it.
+    # and destroy it. Find the object regardless of whether the ark is in the
+    # identifier field or the ark field.
     CSV.parse(file.read, headers: true).each_with_index do |row, _index|
       item = Darlingtonia::InputRecord.from(metadata: row, mapper: CalifornicaMapper.new)
       ark = item.mapper.metadata["Item Ark"]
       Work.where(identifier: ark).each do |work|
+        work&.destroy!
+      end
+      Work.where(ark_sim: ark).each do |work|
         work&.destroy!
       end
     end
