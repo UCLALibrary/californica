d2b9641a94317f7b21b265f11033cc0681287134
Mark Bussey <mark@curationexperts.com>
2019-04-02 15:13:22 -0500

    Reverse the ID value
    This will give us an even b-tree distribution in fedora, which will
    improve performance as the repository grows.
    
    "ModeShape has been designed to efficiently handle a single node having a
    large number (100K-200k) of child nodes out-of-the-box. However, if an
    application stores a lot more nodes than that under a single parent,
    performance in terms of latency and memory consumption starts to
    decrease proportionally to the number of stored children."
    Source:
    https://docs.jboss.org/author/display/MODE40/Large+numbers+of+child+nodes#Largenumbersofchildnodes-Unorderedlargecollections
    
    Since most (all?) of the UCLA ark identifiers start with the same
    string, unless we reverse the ID values we'll have everything stored
    under the same node in Fedora and hit performance issues when the system
    reaches > 200k objects.
    
    This PR also adds unhappy path tests to ARK-->ID generation

--- spec/lib/californica/id_generator_spec.rb
+++ spec/lib/californica/id_generator_spec.rb
@@ -3,8 +3,31 @@ require 'rails_helper'
 
 RSpec.describe Californica::IdGenerator, :clean do
   let(:ark) { 'ark:/13030/t8dn9c0x' }
+  let(:extended_ark) { 'ark:/13030/t8dn9c0x/001' }
+  let(:old_school) { '13030/t8dn9c0x' }
+  let(:wonky_ark) { 'ark:13030/t8dn9c0x' }
+  let(:non_ark) { 'this is not an ark' }
+
   it 'makes a fedora id from an ark' do
     id = Californica::IdGenerator.id_from_ark(ark)
-    expect(id).to eq 't8dn9c0x-13030'
+    expect(id).to eq 'x0c9nd8t-03031'
+  end
+
+  it 'makes an id from arks without prefixes' do
+    id = Californica::IdGenerator.id_from_ark(old_school)
+    expect(id).to eq 'x0c9nd8t-03031'
+  end
+
+  it 'has relaxed rules about prefixes' do
+    id = Californica::IdGenerator.id_from_ark(wonky_ark)
+    expect(id).to eq 'x0c9nd8t-03031'
+  end
+
+  it 'raises an error for arks with extensions' do
+    expect { Californica::IdGenerator.id_from_ark(extended_ark) }.to raise_error(ArgumentError, /ARK/)
+  end
+
+  it 'raises an error for arks that do not have a shoulder & blade' do
+    expect { Californica::IdGenerator.id_from_ark(non_ark) }.to raise_error(ArgumentError, /ARK/)
   end
 end
