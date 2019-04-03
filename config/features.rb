6cb5771bfc5a3cc9d34bfd3757f00828ed2761cf
Bess Sadler <bess@curationexperts.com>
2019-04-03 18:21:01 -0400

    Add a warning to read-only mode
    Warn someone who is enabling read-only mode that they need to leave
    their window open because they won't be able to create a new session
    once read-only mode is on.

--- config/features.rb
+++ config/features.rb
@@ -5,5 +5,5 @@
 Flipflop.configure do
   feature :read_only,
           default: false,
-          description: "Put the system into read-only mode disabling changes and uploads."
+          description: "Put the system into read-only mode disabling changes and uploads. Do not close this session before re-enabling read/write -- you will not be able to login."
 end
