e23ee022ec044daf3bd5f0d577359e113edb3df8
Bess Sadler <bess@curationexperts.com>
2019-04-05 16:30:14 -0400

    Add a styled label for required fields in CSV docs

--- app/helpers/importer_documentation_helper.rb
+++ app/helpers/importer_documentation_helper.rb
@@ -7,9 +7,13 @@
 # Redcarpet is supposed to have a `with_toc_data` option but it appears to be broken.
 class CustomRender < Redcarpet::Render::HTML
   def header(text, header_level)
-    case header_level
-    when 1
-      %(<h#{header_level}>#{text}</h#{header_level}>)
+    return %(<h1>#{text}</h1>) if header_level == 1
+
+    required = text.include?("(required)")
+    text = text.gsub(" (required)", "")
+
+    if required
+      %(<a id=#{text.downcase.tr(' ', '-')}></a><h#{header_level}>#{text}<span class="label label-info required-tag">required</span></h#{header_level}>)
     else
       %(<a id=#{text.downcase.tr(' ', '-')}></a><h#{header_level}>#{text}</h#{header_level}>)
     end
