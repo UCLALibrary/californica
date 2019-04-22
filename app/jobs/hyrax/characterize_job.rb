570d7e056a1fe849e2bbcd6e12cf98c9ba1d1164
Bess Sadler <bess@curationexperts.com>
2019-04-22 17:17:21 -0400

    Log file characterization failures
    Add a custom error for corrupt files
    Run FITS on travis

---
+++ app/jobs/hyrax/characterize_job.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+# Local override of the class at https://github.com/samvera/hyrax/blob/master/app/jobs/characterize_job.rb
+# We are overriding this locally so we can improve and customize the error logging.
+class Hyrax::CharacterizeJob < Hyrax::ApplicationJob
+  queue_as Hyrax.config.ingest_queue_name
+
+  # Characterizes the file at 'filepath' if available, otherwise, pulls a copy from the repository
+  # and runs characterization on that file.
+  # @param [FileSet] file_set
+  # @param [String] file_id identifier for a Hydra::PCDM::File
+  # @param [String, NilClass] filepath the cached file within the Hyrax.config.working_path
+  def perform(file_set, file_id, filepath = nil)
+    raise "#{file_set.class.characterization_proxy} was not found for FileSet #{file_set.id}" unless file_set.characterization_proxy?
+    filepath = Hyrax::WorkingDirectory.find_or_retrieve(file_id, file_set.id) unless filepath && File.exist?(filepath)
+    Hydra::Works::CharacterizationService.run(file_set.characterization_proxy, filepath)
+    Rails.logger.debug "Ran characterization on #{file_set.characterization_proxy.id} (#{file_set.characterization_proxy.mime_type})"
+    file_set.characterization_proxy.save!
+    file_set.update_index
+    file_set.parent&.in_collections&.each(&:update_index)
+    unless file_set.characterization_proxy.mime_type.match?(/tiff/)
+      error = Californica::CorruptFileError.new(file_set_id: file_set.id, mime_type: file_set.characterization_proxy.mime_type)
+      Rails.logger.error error.message
+    end
+    CreateDerivativesJob.perform_later(file_set, file_id, filepath)
+  end
+end
