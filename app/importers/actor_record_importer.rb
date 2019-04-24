d412d7c60fc7472010efdc1e6aabcf68442bde40
Bess Sadler <bess@curationexperts.com>
2019-04-24 13:23:24 -0400

    Used pared down actor stack during import

--- app/importers/actor_record_importer.rb
+++ app/importers/actor_record_importer.rb
@@ -18,42 +18,45 @@ class ActorRecordImporter < Darlingtonia::HyraxRecordImporter
     super(error_stream: error_stream, info_stream: info_stream, attributes: attributes)
   end
 
-  # I am commenting this out for now, so we will go back to using the default Darlingtonia import process, which includes the full actor stack.
-  # Note that this now subclasses Darlingtonia::HyraxRecordImporter, so that is where all of the object creation logic lives.
-  # If we decide to use a sub-set of the actor stack for performance reasons, the below code provides an example of how to do that. -- Bess 22 Feb 2019
-  # private
+  # Create an object using the Hyrax actor stack
+  # We assume the object was created as expected if the actor stack returns true.
+  def create_for(record:)
+    info_stream << "event: record_import_started, batch_id: #{batch_id}, collection_id: #{collection_id}, record_title: #{record.respond_to?(:title) ? record.title : record}"
 
-  # def create_for(record:)
-  #   info_stream << "event: record_import_started, batch_id: #{batch_id}, collection_id: #{collection_id}, record_title: #{record.respond_to?(:title) ? record.title : record}"
-  #
-  #   additional_attrs = {
-  #     depositor: @depositor.user_key
-  #   }
-  #
-  #   created    = import_type.new
-  #
-  #   attributes = record.attributes
-  #
-  #   attrs = record.attributes.merge(additional_attrs)
-  #   attrs = attrs.merge(member_of_collections_attributes: { '0' => { id: collection_id } }) if collection_id
-  #
-  #   based_near = attrs.delete(:based_near)
-  #   attrs = attrs.merge(based_near_attributes: based_near_attributes(based_near)) unless based_near.nil? || based_near.empty?
-  #
-  #   actor_env  = Hyrax::Actors::Environment.new(created,
-  #                                               ::Ability.new(creator),
-  #                                               attributes)
-  #   terminator = Hyrax::Actors::Terminator.new
-  #   middleware = Californica::IngestMiddlewareStack.build_stack.build(terminator)
-  #
-  #   if middleware.create(actor_env)
-  #     info_stream << "event: record_created, batch_id: #{batch_id}, record_id: #{created.id}, collection_id: #{collection_id}, record_title: #{attrs[:title].first}"
-  #     @success_count += 1
-  #   else
-  #     created.errors.each do |attr, msg|
-  #       error_stream << "event: validation_failed, batch_id: #{batch_id}, collection_id: #{collection_id}, attribute: #{attr.capitalize}, message: #{msg}, record_title: #{attrs[:title] ? attrs[:title] : attrs}"
-  #     end
-  #     @failure_count += 1
-  #   end
-  # end
+    additional_attrs = {
+      uploaded_files: create_upload_files(record),
+      depositor: @depositor.user_key,
+      batch_id: batch_id
+    }
+
+    created = import_type.new
+
+    attrs = record.attributes.merge(additional_attrs)
+
+    attrs = attrs.merge(member_of_collections_attributes: { '0' => { id: collection_id } }) if collection_id
+
+    # Ensure nothing is passed in the files field,
+    # since this is reserved for Hyrax and is where uploaded_files will be attached
+    attrs.delete(:files)
+    attrs.delete(:uploaded_files)
+
+    based_near = attrs.delete(:based_near)
+    attrs = attrs.merge(based_near_attributes: based_near_attributes(based_near)) unless based_near.nil? || based_near.empty?
+
+    actor_env = Hyrax::Actors::Environment.new(created,
+                                               ::Ability.new(@depositor),
+                                               attrs)
+    terminator = Hyrax::Actors::Terminator.new
+    middleware = Californica::IngestMiddlewareStack.build_stack.build(terminator)
+
+    if middleware.create(actor_env)
+      info_stream << "event: record_created, batch_id: #{batch_id}, record_id: #{created.id}, collection_id: #{collection_id}, record_title: #{attrs[:title]&.first}"
+      @success_count += 1
+    else
+      created.errors.each do |attr, msg|
+        error_stream << "event: validation_failed, batch_id: #{batch_id}, collection_id: #{collection_id}, attribute: #{attr.capitalize}, message: #{msg}, record_title: record_title: #{attrs[:title] ? attrs[:title] : attrs}"
+      end
+      @failure_count += 1
+    end
+  end
 end
