be1bacc97062411b82252d6ff4f853581eb2f97e
Jamie Little <jamie@jamielittle.org>
2019-05-24 15:11:38 -0500

    Convert the metadata field to JSON

--- spec/models/csv_row_spec.rb
+++ spec/models/csv_row_spec.rb
@@ -2,16 +2,7 @@
 require 'rails_helper'
 
 RSpec.describe CsvRow, type: :model do
-  subject(:csv_row) do 
-    described_class.new(
-    row_number: 1,
-    job_id: '23452',
-    csv_import_id: '193453',
-    status: 'complete',
-    error_messages: 'here is your error',
-    metadata: '{ title: "My Title" }'
-    )
-  end 
+  subject(:csv_row) { FactoryBot.create(:csv_row) }
 
   it 'has a row number' do
     expect(csv_row.row_number).to eq(1)
@@ -23,11 +14,17 @@ RSpec.describe CsvRow, type: :model do
    it 'has a parent csv import record' do
     expect(csv_row.csv_import_id).to eq('193453')
    end
+
+   it 'has a status' do
+    expect(csv_row.status).to eq('complete')
+   end
+
    it 'has a error messages' do
     expect(csv_row.error_messages).to eq('here is your error')
    end
  
-  it 'has metadata' do
-    expect(csv_row.metadata).to eq('{ title: "My Title" }')
-  end
+  it 'has metadata that is parsable as json' do
+    metadata_hash = JSON.parse(csv_row.metadata)
+    expect(metadata_hash['Item Ark']).to eq('21198/zz001pz6jq')
+  end 
 end
