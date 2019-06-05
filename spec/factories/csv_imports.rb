ef8f1d76ce22de1f68d053a95a173d5e5b973bc8
Bess Sadler <bess@curationexperts.com>
2019-06-05 15:49:50 -0400

    Almost working: Parallelized import
    Any error associated with an individual row is now attached to an
    individual background job

--- spec/factories/csv_imports.rb
+++ spec/factories/csv_imports.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 FactoryBot.define do
   factory :csv_import do
-    user { nil }
+    user { FactoryBot.create(:user) }
     manifest { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'csv_import', 'import_manifest.csv'), 'text/csv') }
     import_file_path { Rails.root.join('spec', 'fixtures') }
   end
