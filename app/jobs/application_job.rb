463dcb6134e96d2991cc4b3684adbb3698cef8a6
Jamie Little <jamie@jamielittle.org>
2019-06-17 12:30:50 -0500

    Add sidekiq-status gem
    This gem will give us more details about the status
    of jobs in the sidekiq interface. When this is
    installed there is a Statuses tab in the UI at the
    top of the screen.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 class ApplicationJob < ActiveJob::Base
+  include Sidekiq::Status::Worker
+
+  attr_accessor :jid
+
+  before_perform do |job|
+    job.jid = job.provider_job_id
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'sidekiq/web'
+require 'sidekiq-status/web'
 
 Rails.application.routes.draw do
   get 'importer_documentation/guide'
