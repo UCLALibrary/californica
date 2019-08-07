b6a8637f035c0b8902f1d5860d56c19770760be8
Andrew Wallace <andrewwallace@library.ucla.edu>
2019-08-07 12:31:51 -0700

    helper method to get iiif url stub from access_copy

--- app/services/californica/manifest_builder_service.rb
+++ app/services/californica/manifest_builder_service.rb
@@ -7,6 +7,18 @@ module Californica
       @curation_concern = curation_concern
     end
 
+    def iiif_base_url
+      if @curation_concern.access_copy.nil?
+        nil
+      elsif @curation_concern.access_copy.start_with?(/https?\:\/\//)
+        @curation_concern.access_copy
+      elsif @curation_concern.access_copy.start_with?(/ark\:\//)
+        ENV['IIIF_SERVICE_URL'] + CGI.escape(@curation_concern.access_copy)
+      else
+        ENV['IIIF_SERVER_URL'] + CGI.escape(@curation_concern.access_copy)
+      end
+    end
+
     def image_concerns
       @image_concerns ||= ([@curation_concern] + @curation_concern.ordered_members.to_a).select { |member| member.respond_to?(:access_copy) && member.access_copy }
     end
--- spec/services/californica/manifest_builder_service_spec.rb
+++ spec/services/californica/manifest_builder_service_spec.rb
@@ -3,18 +3,66 @@
 require 'rails_helper'
 
 RSpec.describe Californica::ManifestBuilderService do
-  let(:parent_image_path) { 'parent/image.tif' }
-  let(:child_image_path_1) { 'child/image_1.tif' }
-  let(:child_image_path_2) { 'child/image_2.tif' }
-  let(:child_work1) { FactoryBot.create(:child_work, access_copy: child_image_path_1) }
-  let(:child_work2) { FactoryBot.create(:child_work, access_copy: child_image_path_2) }
+  let(:parent_access_copy) { 'parent/image.tif' }
+  let(:child_access_copy_1) { 'child/image_1.tif' }
+  let(:child_access_copy_2) { 'child/image_2.tif' }
+  let(:child_work1) { FactoryBot.create(:child_work, access_copy: child_access_copy_1) }
+  let(:child_work2) { FactoryBot.create(:child_work, access_copy: child_access_copy_2) }
   let(:work) do
-    w = FactoryBot.create(:work, access_copy: parent_image_path)
+    w = FactoryBot.create(:work, access_copy: parent_access_copy)
     w.ordered_members << child_work1
     w.ordered_members << child_work2
     w
   end
   let(:service) { described_class.new(curation_concern: work) }
+
+  describe '#iiif_base_url' do
+    let(:work) { FactoryBot.create(:work, access_copy: parent_access_copy) }
+
+    context 'when access_copy starts with \'http://\'' do
+      let(:parent_access_copy) { 'http://iiif.library.ucla.edu/iiif/2/finaltest1' }
+
+      it 'uses the link as is' do
+        expect(service.iiif_base_url).to eq 'http://iiif.library.ucla.edu/iiif/2/finaltest1'
+      end
+    end
+    
+    context 'when access_copy starts with \'https://\'' do
+      let(:parent_access_copy) { 'https://iiif.library.ucla.edu/iiif/2/finaltest1' }
+
+      it 'uses the link as is' do
+        expect(service.iiif_base_url).to eq 'https://iiif.library.ucla.edu/iiif/2/finaltest1'
+      end
+    end
+
+    context 'when access_copy starts with \'Masters/\'' do
+      let(:parent_access_copy) { 'Masters/dlmasters/finaltest1' }
+
+      it 'creates a link to the old cantaloupe' do
+        allow(ENV).to receive(:[]).and_call_original
+        allow(ENV).to receive(:[]).with('IIIF_SERVER_URL').and_return('https://old.cantaloupe.url/iiif/2/')
+        expect(service.iiif_base_url).to eq 'https://old.cantaloupe.url/iiif/2/Masters%2Fdlmasters%2Ffinaltest1'
+      end
+    end
+
+    context 'when access_copy starts with \'ark:/\'' do
+      let(:parent_access_copy) { 'ark:/123/456' }
+
+      it 'creates a link to the new cantaloupe' do
+        allow(ENV).to receive(:[]).and_call_original
+        allow(ENV).to receive(:[]).with('IIIF_SERVICE_URL').and_return('https://new.cantaloupe.url/iiif/2/')
+        expect(service.iiif_base_url).to eq 'https://new.cantaloupe.url/iiif/2/ark%3A%2F123%2F456'
+      end
+    end
+
+    context 'when access_copy is nil' do
+      let(:parent_access_copy) { nil }
+
+      it 'returns nil' do
+        expect(service.iiif_base_url).to eq nil
+      end
+    end
+  end
 
   describe '#image_concerns' do
     it 'returns the set of works needed to create a manifest' do
@@ -22,7 +70,7 @@ RSpec.describe Californica::ManifestBuilderService do
     end
 
     context 'When the parent work has no access_copy' do
-      let(:parent_image_path) { nil }
+      let(:parent_access_copy) { nil }
 
       it 'returns the set of child works' do
         expect(service.image_concerns).to eq [child_work1, child_work2]
@@ -30,7 +78,7 @@ RSpec.describe Californica::ManifestBuilderService do
     end
 
     context 'When the parent work has no children' do
-      let(:work) { FactoryBot.create(:work, access_copy: parent_image_path) }
+      let(:work) { FactoryBot.create(:work, access_copy: parent_access_copy) }
 
       it 'returns only the parent' do
         expect(service.image_concerns).to eq [work]
@@ -46,7 +94,7 @@ RSpec.describe Californica::ManifestBuilderService do
     end
 
     context 'When the a child work has no access_copy' do
-      let(:child_image_path_1) { nil }
+      let(:child_access_copy_1) { nil }
 
       it 'does not include the child' do
         expect(service.image_concerns).to eq [work, child_work2]
