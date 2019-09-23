b4903073b43bb9dfbf75fa62d80c29607e01437d
Andrew Wallace <andrewwallace@library.ucla.edu>
2019-09-23 12:43:00 -0700

    Update parent manifest on GUI create/update/delete

--- app/actors/californica/manifest_actor.rb
+++ app/actors/californica/manifest_actor.rb
@@ -16,13 +16,29 @@ module Californica
 
     private
 
+      # Build the IIIF manifest for the work. If a @curation_concern is available in env, also build manifests for any parent works.
       def build_manifest(env)
-        CreateManifestJob.perform_later(env.attributes['ark'])
+        if env.curation_concern
+          build_manifest_for_parent(env.curation_concern)
+        else
+          CreateManifestJob.perform_later(env.attributes['ark'])
+        end
+      end
+
+      def build_manifest_for_parent(curation_concern)
+        return unless curation_concern.is_a? Hyrax::WorkBehavior
+        CreateManifestJob.perform_later(curation_concern.ark)
+        curation_concern.member_of.each do |parent|
+          build_manifest_for_parent(parent)
+        end
       end
 
       def delete_manifest(env)
         manifest_url = env.curation_concern.iiif_manifest_url
         HTTParty.delete(manifest_url) if manifest_url
+        env.curation_concern.member_of.each do |parent|
+          build_manifest_for_parent(parent)
+        end
       end
   end
 end
--- spec/actors/californica/manifest_actor_spec.rb
+++ spec/actors/californica/manifest_actor_spec.rb
@@ -4,6 +4,12 @@ require 'rails_helper'
 RSpec.describe Californica::ManifestActor do
   let(:actor) { described_class.new(next_actor) }
   let(:attrs) { { 'ark' => 'ark:/abc/123' } }
+  let(:child_attrs) { { 'ark' => 'ark:/abc/456' } }
+  let(:child_work) do
+    child = FactoryBot.build(:child_work, child_attrs)
+    allow(child).to receive(:member_of).and_return([work])
+    child
+  end
   let(:env) { instance_double('Hyrax::Actors::Environment', curation_concern: work, attributes: attrs) }
   let(:next_actor) { instance_double('Hyrax::Actors::BaseActor', create: true, update: true, destroy: true) }
   let(:work) { FactoryBot.build(:work, attrs) }
@@ -11,9 +17,42 @@ RSpec.describe Californica::ManifestActor do
   before { ActiveJob::Base.queue_adapter = :test }
 
   describe '#create' do
+    before { actor.create(env) }
+
     it 'enqueues a CreateManifestJob' do
-      actor.create(env)
-      expect(CreateManifestJob).to have_been_enqueued
+      expect(CreateManifestJob).to have_been_enqueued.with('ark:/abc/123')
+    end
+
+    context 'when there is a parent work' do
+      let(:env) { instance_double('Hyrax::Actors::Environment', curation_concern: child_work, attributes: child_attrs) }
+
+      it 'enqueues a CreateManifestJob for the child work' do
+        expect(CreateManifestJob).to have_been_enqueued.with('ark:/abc/456')
+      end
+
+      it 'enqueues a CreateManifestJob for the parent' do
+        expect(CreateManifestJob).to have_been_enqueued.with('ark:/abc/123')
+      end
+    end
+
+    context 'when there is a parent collection' do
+      let(:env) do
+        collection = FactoryBot.build(:collection, ark: 'ark/xyz/321')
+        allow(work).to receive(:member_of).and_return([collection])
+        instance_double('Hyrax::Actors::Environment', curation_concern: work, attributes: attrs)
+      end
+
+      it 'does not enque a CreateManifestJob for the collection' do
+        expect(CreateManifestJob).not_to have_been_enqueued.with('ark/xyz/321')
+      end
+    end
+
+    context 'when env.curation_concern is nil' do
+      let(:env) { instance_double('Hyrax::Actors::Environment', curation_concern: nil, attributes: attrs) }
+
+      it 'uses the ark from env.attributes' do
+        expect(CreateManifestJob).to have_been_enqueued.with('ark:/abc/123')
+      end
     end
   end
 
@@ -39,12 +78,54 @@ RSpec.describe Californica::ManifestActor do
         expect(stubbed_request).not_to have_been_requested
       end
     end
+
+    context 'when there is a parent work' do
+      let(:env) { instance_double('Hyrax::Actors::Environment', curation_concern: child_work, attributes: child_attrs) }
+
+      it 'enqueues a CreateManifestJob for the parent' do
+        actor.destroy(env)
+        expect(CreateManifestJob).to have_been_enqueued.with('ark:/abc/123')
+      end
+    end
   end
 
   describe '#update' do
+    before { actor.update(env) }
+
     it 'enqueues a CreateManifestJob' do
-      actor.create(env)
-      expect(CreateManifestJob).to have_been_enqueued
+      expect(CreateManifestJob).to have_been_enqueued.with('ark:/abc/123')
+    end
+
+    context 'when there is a parent work' do
+      let(:env) { instance_double('Hyrax::Actors::Environment', curation_concern: child_work, attributes: child_attrs) }
+
+      it 'enqueues a CreateManifestJob for the child work' do
+        expect(CreateManifestJob).to have_been_enqueued.with('ark:/abc/456')
+      end
+
+      it 'enqueues a CreateManifestJob for the parent' do
+        expect(CreateManifestJob).to have_been_enqueued.with('ark:/abc/123')
+      end
+    end
+
+    context 'when there is a parent collection' do
+      let(:env) do
+        collection = FactoryBot.build(:collection, ark: 'ark/xyz/321')
+        allow(work).to receive(:member_of).and_return([collection])
+        instance_double('Hyrax::Actors::Environment', curation_concern: work, attributes: attrs)
+      end
+
+      it 'does not enque a CreateManifestJob for the collection' do
+        expect(CreateManifestJob).not_to have_been_enqueued.with('ark/xyz/321')
+      end
+    end
+
+    context 'when env.curation_concern is nil' do
+      let(:env) { instance_double('Hyrax::Actors::Environment', curation_concern: nil, attributes: attrs) }
+
+      it 'uses the ark from env.attributes' do
+        expect(CreateManifestJob).to have_been_enqueued.with('ark:/abc/123')
+      end
     end
   end
 end
