aee6cd4844d7336ccc89e96d6296d6cd8a81bcf4
Andrew Wallace <andrewwallace@library.ucla.edu>
2019-10-08 13:56:24 -0700

    Allow deduplicated jobs
    Adds infrastructure to ApplicationJob such that its subclasses may define a method 'deduplication_key', with the result that if a job has already been enqueued with the same deduplication_key, it will not be enqueued again.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -6,5 +6,27 @@ class ApplicationJob < ActiveJob::Base
 
   before_perform do |job|
     job.jid = job.provider_job_id
+    redis = Redis.current
+    redis.set(redis_key, 'false')
+  end
+
+  around_enqueue do |_, block|
+    if deduplication_key
+      redis = Redis.current
+      unless redis.get(redis_key) == 'true'
+        redis.set(redis_key, 'true')
+        block.call
+      end
+    else
+      block.call
+    end
+  end
+
+  def deduplication_key
+    nil
+  end
+
+  def redis_key
+    (@redis_key ||= "in_queue_#{deduplication_key}") if deduplication_key
   end
 end
