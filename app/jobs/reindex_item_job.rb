6303e4e9bcd4de6f9fb83553e337172b3ed5123a
Andrew Wallace <andrewwallace@library.ucla.edu>
2019-10-10 15:23:42 -0700

    During ReindexItemJob, apply page order and enqueue CreateManifestJob

--- app/jobs/reindex_item_job.rb
+++ app/jobs/reindex_item_job.rb
@@ -6,18 +6,27 @@ class ReindexItemJob < ApplicationJob
   def perform(item_ark, csv_import_id: nil)
     item = Collection.find_by_ark(item_ark) || Work.find_by_ark(item_ark) || ChildWork.find_by_ark(item_ark)
     raise(ArgumentError, "No such item: #{item_ark}.") unless item
-    set_recalculate_size(item)
+
+    # Apply page orderings if importing Works from a CSV
+    if csv_import_id && item.is_a?(Work)
+      page_orderings = PageOrder.where(parent: Ark.ensure_prefix(item_ark))
+      ordered_arks = page_orderings.sort_by(&:sequence)
+      item.ordered_members = ordered_arks.map { |b| ChildWork.find_by_ark(b.child) }
+    end
+
+    enable_recalculate_size(item)
     item.save
+
+    CreateManifestJob.perform_later(item_ark) unless item.is_a? Collection
   end
 
   def deduplication_key
     "ReindexItemJob-#{arguments[0]}"
   end
 
-  def set_recalculate_size(item)
+  def enable_recalculate_size(item)
     item.recalculate_size = true
   rescue NoMethodError
     nil
   end
-
 end
--- spec/jobs/reindex_item_job_spec.rb
+++ spec/jobs/reindex_item_job_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe ReindexItemJob, type: :job do
   before do
     allow(item).to receive(:save)
   end
-  
+
   it 'sets recalculate_size=true' do
     allow(Collection).to receive(:find_by_ark).with(item.ark).and_return(item)
     described_class.perform_now(item.ark)
     expect(item.recalculate_size).to be true
   end
-  
+
   it 'calls save to trigger a reindex' do
     allow(Collection).to receive(:find_by_ark).with(item.ark).and_return(item)
     described_class.perform_now(item.ark)
@@ -21,29 +21,77 @@ RSpec.describe ReindexItemJob, type: :job do
   end
 
   it 'gets deduplicated' do
+    allow(Collection).to receive(:find_by_ark).with(item.ark).and_return(item)
+    described_class.perform_now(item.ark)
     expect do
       described_class.perform_later(item.ark)
       described_class.perform_later(item.ark)
     end.to have_enqueued_job(described_class).exactly(:once)
   end
-  
+
+  context 'when the item is a collection' do
+    before { allow(Collection).to receive(:find_by_ark).with(item.ark).and_return(item) }
+
+    it 'does not enqueue a CreateManifestJob' do
+      expect do
+        described_class.perform_now(item.ark)
+      end.not_to have_enqueued_job(CreateManifestJob).with(item.ark)
+    end
+  end
+
   context 'when the item is a Work' do
     let(:item) { FactoryBot.build(:work) }
+    before { allow(Work).to receive(:find_by_ark).with(item.ark).and_return(item) }
 
     it 'calls save to trigger a reindex' do
-      allow(Work).to receive(:find_by_ark).with(item.ark).and_return(item)
       described_class.perform_now(item.ark)
       expect(item).to have_received(:save)
     end
+
+    it 'enqueues a CreateManifestJob' do
+      expect do
+        described_class.perform_now(item.ark)
+      end.to have_enqueued_job(CreateManifestJob).with(item.ark)
+    end
+
+    context 'when the Work has children' do
+      let(:child1) { FactoryBot.build(:child_work) }
+      let(:child2) { FactoryBot.build(:child_work) }
+      let(:child3) { FactoryBot.build(:child_work) }
+      let(:page_order_objects) do
+        [PageOrder.new(parent: item.ark, child: child2.ark, sequence: 2),
+         PageOrder.new(parent: item.ark, child: child1.ark, sequence: 1),
+         PageOrder.new(parent: item.ark, child: child3.ark, sequence: 3)]
+      end
+
+      before do
+        allow(PageOrder).to receive(:where).with(parent: item.ark).and_return(page_order_objects)
+        allow(ChildWork).to receive(:find_by_ark).with(child1.ark).and_return(child1)
+        allow(ChildWork).to receive(:find_by_ark).with(child2.ark).and_return(child2)
+        allow(ChildWork).to receive(:find_by_ark).with(child3.ark).and_return(child3)
+        # allow(item).to receive(:save).and_call_original
+      end
+
+      it 'applies the page order' do
+        described_class.perform_now(item.ark, csv_import_id: 123)
+        expect(item.ordered_member_ids).to eq [child1.id, child2.id, child3.id]
+      end
+    end
   end
-  
+
   context 'when the item is a ChildWork' do
     let(:item) { FactoryBot.build(:work) }
+    before { allow(ChildWork).to receive(:find_by_ark).with(item.ark).and_return(item) }
 
     it 'calls save to trigger a reindex' do
-      allow(ChildWork).to receive(:find_by_ark).with(item.ark).and_return(item)
       described_class.perform_now(item.ark)
       expect(item).to have_received(:save)
+    end
+
+    it 'enqueues a CreateManifestJob' do
+      expect do
+        described_class.perform_now(item.ark)
+      end.to have_enqueued_job(CreateManifestJob).with(item.ark)
     end
   end
 
