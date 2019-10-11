f7b33d791dd734dccdeaa4939fed692bc9355779
Andrew Wallace <andrewwallace@library.ucla.edu>
2019-10-11 12:46:00 -0700

    Record job durations to CsvImportTask record

--- app/jobs/create_manifest_job.rb
+++ app/jobs/create_manifest_job.rb
@@ -3,9 +3,40 @@
 class CreateManifestJob < ApplicationJob
   queue_as :default
 
-  def perform(work_ark)
+  def perform(work_ark, csv_import_task_id: nil)
+    log_start(csv_import_task_id)
+
     work = Work.find_by_ark(work_ark) || ChildWork.find_by_ark(work_ark)
     raise(ArgumentError, "No such Work or ChildWork: #{work_ark}.") unless work
+
     Californica::ManifestBuilderService.new(curation_concern: work).persist
+
+    log_end(csv_import_task_id)
   end
+
+  private
+
+    def log_start(csv_import_task_id)
+      return unless csv_import_task_id
+      @start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      csv_import_task = CsvImportTask.find(csv_import_task_id)
+      csv_import_task.job_status = 'In Progress'
+      begin
+        csv_import_task.times_started += 1
+      rescue NoMethodError
+        csv_import_task = 0
+      end
+      csv_import_task.start_timestamp = @start_time
+      csv_import_task.save
+    end
+
+    def log_end(csv_import_task_id)
+      return unless csv_import_task_id
+      csv_import_task = CsvImportTask.find(csv_import_task_id)
+      @end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      csv_import_task.end_timestamp = @end_time
+      csv_import_task.job_duration = @end_time - @start_time
+      csv_import_task.job_status = 'Complete'
+      csv_import_task.save
+    end
 end
--- app/jobs/reindex_item_job.rb
+++ app/jobs/reindex_item_job.rb
@@ -3,12 +3,14 @@
 class ReindexItemJob < ApplicationJob
   queue_as :default
 
-  def perform(item_ark, csv_import_id: nil)
+  def perform(item_ark, csv_import_task_id: nil)
+    log_start(csv_import_task_id)
+
     item = Collection.find_by_ark(item_ark) || Work.find_by_ark(item_ark) || ChildWork.find_by_ark(item_ark)
     raise(ArgumentError, "No such item: #{item_ark}.") unless item
 
     # Apply page orderings if importing Works from a CSV
-    if csv_import_id && item.is_a?(Work)
+    if csv_import_task_id && item.is_a?(Work)
       page_orderings = PageOrder.where(parent: Ark.ensure_prefix(item_ark))
       ordered_arks = page_orderings.sort_by(&:sequence)
       item.ordered_members = ordered_arks.map { |b| ChildWork.find_by_ark(b.child) }
@@ -18,15 +20,43 @@ class ReindexItemJob < ApplicationJob
     item.save
 
     CreateManifestJob.perform_later(item_ark) unless item.is_a? Collection
+
+    log_end(csv_import_task_id)
   end
 
   def deduplication_key
     "ReindexItemJob-#{arguments[0]}"
   end
 
-  def enable_recalculate_size(item)
-    item.recalculate_size = true
-  rescue NoMethodError
-    nil
-  end
+  private
+
+    def log_start(csv_import_task_id)
+      return unless csv_import_task_id
+      @start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      csv_import_task = CsvImportTask.find(csv_import_task_id)
+      csv_import_task.job_status = 'In Progress'
+      begin
+        csv_import_task.times_started += 1
+      rescue NoMethodError
+        csv_import_task = 0
+      end
+      csv_import_task.start_timestamp = @start_time
+      csv_import_task.save
+    end
+
+    def log_end(csv_import_task_id)
+      return unless csv_import_task_id
+      csv_import_task = CsvImportTask.find(csv_import_task_id)
+      @end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      csv_import_task.end_timestamp = @end_time
+      csv_import_task.job_duration = @end_time - @start_time
+      csv_import_task.job_status = 'Complete'
+      csv_import_task.save
+    end
+
+    def enable_recalculate_size(item)
+      item.recalculate_size = true
+    rescue NoMethodError
+      nil
+    end
 end
