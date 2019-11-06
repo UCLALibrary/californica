aef0b46679cc8a060c6a0c1743322d6b3fc20f39
Andrew Wallace <andrewwallace@library.ucla.edu>
2019-11-06 11:44:19 -0800

    validate uniqueness CsvRow.row_number, csv_import

--- app/models/csv_row.rb
+++ app/models/csv_row.rb
@@ -4,4 +4,5 @@ class CsvRow < ApplicationRecord
   serialize :job_ids_completed, Set
   serialize :job_ids_errored, Set
   serialize :error_messages, Array
+  validates :row_number, uniqueness: { scope: :csv_import_id, message: 'row_number and csv_import must be unique together' }
 end
