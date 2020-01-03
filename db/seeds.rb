fc7b6101c2c6dd3f64fb4aca5ec9ac37a3d49a51
Hardy Pottinger <hpottinger@library.ucla.edu>
2020-01-03 16:32:54 -0600

    [CAL-859] Only create a default admin in a dev environment (#775)
    * [CAL-859] Only create a default admin user in Dev environments
    
    * [CAL-859] Also create an admin user for the test environment, heh

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# Set up a default admin user
-u = User.find_or_create_by(email: ENV['ADMIN_EMAIL'] || 'admin@example.com')
-u.display_name = "Default Admin"
-u.password = ENV['ADMIN_PASSWORD'] || 'password'
-u.save
-admin_role = Role.find_or_create_by(name: 'admin')
-admin_role.users << u
-admin_role.save
+
+# Set up a default admin user, if we are in a Development environment, otherwise, skip
+if Rails.env.development? || Rails.env.test? 
+    u = User.find_or_create_by(email: ENV['ADMIN_EMAIL'] || 'admin@example.com')
+    u.display_name = "Default Admin"
+    u.password = ENV['ADMIN_PASSWORD'] || 'password'
+    u.save
+    admin_role = Role.find_or_create_by(name: 'admin')
+    admin_role.users << u
+    admin_role.save
+end
