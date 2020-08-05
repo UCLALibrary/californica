a70b5e4951476d4e63e15a8543e06be63f250746
Parinita Mulak <pghorpade@library.ucla.edu>
2020-08-05 15:26:04 -0700

    Use Solrj classes to write to ursus solr core

--- solr/config/update-script.js
+++ solr/config/update-script.js
@@ -12,29 +12,57 @@ function processAdd(cmd) {
   id = doc.getFieldValue('id');
   logger.info('update-script#processAdd: id=' + id);
 
-  title = doc.getFieldValue('title');
+  title = doc.getFieldValue('title_tesim');
   logger.info('update-script#processAdd: title=' + title);
 
   hash_id_ssi = doc.getFieldValue('hashed_id_ssi');
   logger.info('update-script#processAdd: hashed_id_ssi=' + hash_id_ssi);
   //xmlHttp = new XMLHttpRequest();
-  var URL = Java.type('java.net.URL');
-  url = new URL('http://example.com');
-  logger.info('update-script#processAdd: URL=' + url);
+  //var URL = Java.type('java.net.URL');
+  //url = new URL('http://example.com');
+  //logger.info('update-script#processAdd: URL=' + url);
   logger.info('update-script#processAdd: doc=' + doc);
-  var SolrInputDocument = Java.type('org.apache.solr.common.SolrInputDocument');
-  solrDocUrsus = new SolrInputDocument();
-  solrDocUrsus.addField('id', doc.getFieldValue('ursus_id_ssi'));
-  logger.info('update-script#processAdd: solrDocUrsus start=' + solrDocUrsus);
-  logger.info('update-script#processAdd: Set=' + doc.entrySet());
-  field_names = doc.getFieldNames().toArray();
-  for (i = 0; i < field_names.length; i++) {
-    field_name = field_names[i];
-    if (field_name != 'id') {
-      solrDocUrsus.addField(field_names[i], doc.getFieldValue(field_names[i]));
+  if (
+    doc.getFieldValue('has_model_ssim') == 'Work' ||
+    doc.getFieldValue('has_model_ssim') == 'Collection'
+  ) {
+    var SolrInputDocument = Java.type(
+      'org.apache.solr.common.SolrInputDocument'
+    );
+    solrDocUrsus = new SolrInputDocument();
+    solrDocUrsus.addField('id', doc.getFieldValue('ark_ssi'));
+    logger.info('update-script#processAdd: solrDocUrsus start=' + solrDocUrsus);
+    logger.info('update-script#processAdd: Set=' + doc.entrySet());
+    field_names = doc.getFieldNames().toArray();
+    for (i = 0; i < field_names.length; i++) {
+      field_name = field_names[i];
+      if (field_name != 'id') {
+        solrDocUrsus.addField(
+          field_names[i],
+          doc.getFieldValue(field_names[i])
+        );
+      }
     }
+    logger.info(
+      'update-script#processAdd: solrDocUrsus finish=' + solrDocUrsus
+    );
+    var SolrClient = Java.type('org.apache.solr.client.solrj.SolrClient');
+    var HttpSolrClient = Java.type(
+      'org.apache.solr.client.solrj.impl.HttpSolrClient'
+    );
+    var XMLResponseParser = Java.type(
+      'org.apache.solr.client.solrj.impl.XMLResponseParser'
+    );
+
+    urlString = 'http://localhost:8983/solr/ursus';
+    logger.info('update-script#processAdd: SolrClient=' + SolrClient);
+    solrclient = new HttpSolrClient.Builder(urlString).build();
+    solrclient.setParser(new XMLResponseParser());
+    logger.info('update-script#processAdd: solrclient=' + solrclient);
+    solrclient.add(solrDocUrsus);
+    solrclient.commit();
   }
-  logger.info('update-script#processAdd: solrDocUrsus finish=' + solrDocUrsus);
+
   //var solrDocUrsus = doc.deepcopy();
   //solrDocUrsus.setField('id', doc.getFieldValue('ursus_id_ssi'));
   //logger.info('update-script#processAdd: solrDocUrsus=' + solrDocUrsus);
@@ -55,6 +83,7 @@ function processAdd(cmd) {
   //    field_name = field_names[i];
   //    if (/attr_.*/.test(field_name)) { doc.addField("attribute_ss", field_names[i]); }
   //  }
+}
 
 function processDelete(cmd) {
   // no-op
