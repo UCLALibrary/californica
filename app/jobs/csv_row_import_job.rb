13e53d218532287f3e7894b6a83aba2aa3662f52
darrowcoucla <darrowco@library.ucla.edu>
2020-10-26 14:07:45 -0700

    fixed test that broke

--- app/jobs/csv_row_import_job.rb
+++ app/jobs/csv_row_import_job.rb
@@ -12,42 +12,44 @@ class CsvRowImportJob < ActiveJob::Base
 
     @metadata = JSON.parse(@row.metadata)
 
-    # build the backwards ark to query Fedora
-    fedoraArk2 = @metadata["Item ARK"]
-    fedoraArk3 = fedoraArk2.sub("ark:/","")
-    fedoraArk4 = fedoraArk3.sub("/","-")
-    fedoraArk5 = fedoraArk4.reverse
-    fedoraArk6a = fedoraArk5.split("-")
-    fedoraArk6 = fedoraArk6a.first()
-    fedoraArk7 = fedoraArk6.scan /\w/
-    fedoraArk8 = ""
-    arkCnt = 0
-    fedoraArk7.each do |arkL|
-      if arkCnt.modulo(2) == 0
-        fedoraArk8 += "/"
+    unless @metadata["Item ARK"] == ""
+      # build the backwards ark to query Fedora
+      fedoraArk2 = @metadata["Item ARK"]
+      fedoraArk3 = fedoraArk2.sub("ark:/","")
+      fedoraArk4 = fedoraArk3.sub("/","-")
+      fedoraArk5 = fedoraArk4.reverse
+      fedoraArk6a = fedoraArk5.split("-")
+      fedoraArk6 = fedoraArk6a.first()
+      fedoraArk7 = fedoraArk6.scan /\w/
+      fedoraArk8 = ""
+      arkCnt = 0
+      fedoraArk7.each do |arkL|
+        if arkCnt.modulo(2) == 0
+          fedoraArk8 += "/"
+        end
+        fedoraArk8 += arkL
+        arkCnt += 1
       end
-      fedoraArk8 += arkL
-      arkCnt += 1
-    end
-    fedoraArk8 += "/"
-    fedoraArk9 = fedoraArk8 + fedoraArk5
-    fedoraArk10 = fedoraArk9.sub("/zz/","/")
-
-    # build Fedora URI and remove tombstone if present
-    url = "#{ActiveFedora.config.credentials[:url]}#{ActiveFedora.config.credentials[:base_path]}"
-    fedoraArk11 = url + fedoraArk10
-    uri = URI(fedoraArk11)
-    fedoraResp = Net::HTTP.get_response(uri)
-
-    if fedoraResp.body["ombstone"]
-      fedoraArk11 = fedoraArk11 + "/fcr:tombstone"
+      fedoraArk8 += "/"
+      fedoraArk9 = fedoraArk8 + fedoraArk5
+      fedoraArk10 = fedoraArk9.sub("/zz/","/")
+  
+      # build Fedora URI and remove tombstone if present
+      url = "#{ActiveFedora.config.credentials[:url]}#{ActiveFedora.config.credentials[:base_path]}"
+      fedoraArk11 = url + fedoraArk10
       uri = URI(fedoraArk11)
-      http = Net::HTTP.new(uri.host, uri.port)
-      req = Net::HTTP::Delete.new(uri.path)
-      req.basic_auth ActiveFedora.config.credentials[:user], ActiveFedora.config.credentials[:password]
-      http.request(req)
-      tombstoneMsgAry = Array.new(1, "tombstone removed")
-      @row.error_messages = tombstoneMsgAry
+      fedoraResp = Net::HTTP.get_response(uri)
+  
+      if fedoraResp.body["ombstone"]
+        fedoraArk11 = fedoraArk11 + "/fcr:tombstone"
+        uri = URI(fedoraArk11)
+        http = Net::HTTP.new(uri.host, uri.port)
+        req = Net::HTTP::Delete.new(uri.path)
+        req.basic_auth ActiveFedora.config.credentials[:user], ActiveFedora.config.credentials[:password]
+        http.request(req)
+        tombstoneMsgAry = Array.new(1, "tombstone removed")
+        @row.error_messages = tombstoneMsgAry
+      end
     end
 
     @row.status = @metadata["Object Type"].include?("Page") ? 'not ingested' : 'in progress'
