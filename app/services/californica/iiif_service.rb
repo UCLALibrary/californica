7f019d244dd9a604dd42bed6e8895438b7a1b0d9
Ashton Prigge <ashton.prigge@gmail.com>
2020-11-24 15:29:47 -0800

    CAL-984

--- app/services/californica/iiif_service.rb
+++ app/services/californica/iiif_service.rb
@@ -1,16 +1,20 @@
 module Californica
     class IiifService
-        def src(iiif_manifest_url)
+        def http_sub(iiif_manifest_url)
             iiif_manifest_url.sub('http:', 'https:')
+        end
+        def src(iiif_manifest_url)
             if ENV['RAILS_HOST'] == 'localhost' || ENV['RAILS_HOST'] == 'californica-test.library.ucla.edu'
-                "https://t-w-dl-viewer01.library.ucla.edu/uv.html#?manifest=#{CGI.escape(iiif_manifest_url)}"
+                "https://t-w-dl-viewer01.library.ucla.edu/uv.html#?manifest=#{CGI.escape(http_sub(iiif_manifest_url))}"
             elsif ENV['RAILS_HOST'] == 'californica-dev.library.ucla.edu'
-                "https://d-w-dl-viewer01.library.ucla.edu/uv.html#?manifest=#{CGI.escape(iiif_manifest_url)}"
+                "https://d-w-dl-viewer01.library.ucla.edu/uv.html#?manifest=#{CGI.escape(http_sub(iiif_manifest_url))}"
             elsif ENV['RAILS_HOST'] == 'californica-stage.library.ucla.edu'
-                "https://s-w-dl-viewer01.library.ucla.edu/uv.html#?manifest=#{CGI.escape(iiif_manifest_url)}"
+                "https://s-w-dl-viewer01.library.ucla.edu/uv.html#?manifest=#{CGI.escape(http_sub(iiif_manifest_url))}"
             elsif ENV['RAILS_HOST'] == 'californica.library.ucla.edu'
-                "https://p-w-dl-viewer01.library.ucla.edu/uv.html#?manifest=#{CGI.escape(iiif_manifest_url)}"
+                "https://p-w-dl-viewer01.library.ucla.edu/uv.html#?manifest=#{CGI.escape(http_sub(iiif_manifest_url))}"
             end
         end
     end
 end
+
+
