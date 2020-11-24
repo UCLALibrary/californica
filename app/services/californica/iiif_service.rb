47fbe24363807ffadca1051ed8f57550a8193395
Ashton Prigge <ashton.prigge@gmail.com>
2020-11-24 13:24:03 -0800

    CAL-984: Sub https for http in iiif_manifest url.

--- app/services/californica/iiif_service.rb
+++ app/services/californica/iiif_service.rb
@@ -1,6 +1,7 @@
 module Californica
     class IiifService
         def src(iiif_manifest_url)
+            iiif_manifest_url.sub('http:', 'https:')
             if ENV['RAILS_HOST'] == 'localhost' || ENV['RAILS_HOST'] == 'californica-test'
                 "https://t-w-dl-viewer01.library.ucla.edu/uv.html#?manifest=#{CGI.escape(iiif_manifest_url)}"
             elsif ENV['RAILS_HOST'] == 'californica-dev'
