04778c87e4061a243687139c11b15b9d03e78675
Ashton Prigge <ashton.prigge@gmail.com>
2020-11-24 14:25:30 -0800

    CAL-984

--- app/services/californica/iiif_service.rb
+++ app/services/californica/iiif_service.rb
@@ -2,7 +2,14 @@ module Californica
     class IiifService
         def src(iiif_manifest_url)
             iiif_manifest_url.sub('http:', 'https:')
-            "https://t-w-dl-viewer01.library.ucla.edu/uv.html#?manifest=#{CGI.escape(iiif_manifest_url)}"
+            if ENV['RAILS_HOST'] == 'localhost' || ENV['RAILS_HOST'] == 'californica-test'
+                "https://t-w-dl-viewer01.library.ucla.edu/uv.html#?manifest=#{CGI.escape(iiif_manifest_url)}"
+            elsif ENV['RAILS_HOST'] == 'californica-dev'
+                "https://d-w-dl-viewer01.library.ucla.edu/uv.html#?manifest=#{CGI.escape(iiif_manifest_url)}"
+            elsif ENV['RAILS_HOST'] == 'californica-stage'
+                "https://s-w-dl-viewer01.library.ucla.edu/uv.html#?manifest=#{CGI.escape(iiif_manifest_url)}"
+            elsif ENV['RAILS_HOST'] == 'californica'
+                "https://p-w-dl-viewer01.library.ucla.edu/uv.html#?manifest=#{CGI.escape(iiif_manifest_url)}"
             end
         end
     end
