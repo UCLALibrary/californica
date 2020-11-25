5f88a8b92c052548c2eb2b7969d0518dc928eab5
Ashton Prigge <ashton.prigge@gmail.com>
2020-11-25 09:16:45 -0800

    CAL-984: Fix test errors.

--- app/services/californica/iiif_service.rb
+++ app/services/californica/iiif_service.rb
@@ -5,6 +5,7 @@ module Californica
     def http_sub(iiif_manifest_url)
       iiif_manifest_url.sub('http:', 'https:')
     end
+
     def src(iiif_manifest_url)
       if ENV['RAILS_HOST'] == 'localhost' || ENV['RAILS_HOST'] == 'californica-test.library.ucla.edu'
         "https://t-w-dl-viewer01.library.ucla.edu/uv.html#?manifest=#{CGI.escape(http_sub(iiif_manifest_url))}"
