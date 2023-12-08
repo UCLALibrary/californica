ab1a3245152cbbf4616c6497a8acaf3a4f8aeb41
jendiamond <thejendiamond@gmail.com>
2023-12-07 17:40:08 -0800

    re-add engraver_tesim to catalog_controller

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -137,6 +137,7 @@ class CatalogController < ApplicationController
     config.add_show_field 'director_tesim'
     config.add_show_field 'edition_ssm', label: 'Edition'
     config.add_show_field 'editor_tesim'
+    config.add_show_field 'engraver_tesim'
     config.add_show_field 'extent_tesim'
     config.add_show_field 'finding_aid_url_ssm'
     config.add_show_field 'foliation_ssi', label: 'Foliation note'
