04962d1c62a96a03ec062b0d680500f1641e7d4a
Andy Wallace <andrewwallace@library.ucla.edu>
2025-02-14 10:52:57 -0800

    Update catalog_controller.rb

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -214,7 +214,7 @@ class CatalogController < ApplicationController
     config.add_show_field 'inscription_tesim'
     config.add_show_field 'librettist_tesim'
     config.add_show_field 'script_tesim'
-    config.add_show_field 'services_contact'
+    config.add_show_field 'services_contact_ssm'
 
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
